e7d9cea9e81910d251cc9c62a013ee71a3c1698b
maraw6599 <n_solovy@yahoo.com>
2019-02-12 19:06:45 -0500

    upgraded

--- lab4/Car.java
+++ lab4/Car.java
@@ -1,65 +1,66 @@
+/**
+ * Defines the Car object: model, location, speed, moving direction 
+ * @author   Natalia Solovyeva 
+ *  @version  12/02/2019  
+ */
 public class Car {
- private String model;
- private int location;
- private int currentSpeed;
- private boolean movingForward;
- private int maxSpeed;
- 
- // constructor creates a car object
- public Car (String model, int maxSpeed, int location){
-   this.model = model;
-   this.maxSpeed = maxSpeed;
-   this.location = location;
-   this.movingForward = true;
-   this.currentSpeed = 0;
- }
- //getters
- public String getModel(){
-   return this.model;
- }
- 
- public boolean getDirection(){
-   return this.movingForward;
- }
- 
- public int getLocation(){
-    return this.location;
- }
-//method sets the current speeed of car to be the object maxSpeed
-   public int go(){
-     return currentSpeed = maxSpeed;  
+   private String model;
+   private int location;
+   private int currentSpeed;
+   private boolean movingForward;
+   private int maxSpeed;
+   private boolean changedDirection;
+   
+   // constructor creates a car object
+   Car(String model, int maxSpeed, int location){
+       this.model = model;
+       this.location = location;
+       this.maxSpeed = maxSpeed;
+       this.movingForward = true;
+       this.currentSpeed = 0;  
+       this.changedDirection = false;
    }
-//method sets the current speed of the car to 0
-   public int stop(){
-       return currentSpeed = 0;
+   // getters
+    String getModel(){
+       return this.model;
    }
-   public boolean turnAround(){
-     if (this.movingForward){
-       this.movingForward = false;
-     }else{
-         this.movingForward = true;
-       }
-     return movingForward;
+   boolean getDirection(){
+       return this.movingForward;
    }
-//change the location: add or subtract the currentSpeed from the location
-   public int continu(){
-     if (this.movingForward = true){
-       this.location += this.currentSpeed;
-     }
-     if (this.movingForward = false) {
-       this.location -= this.currentSpeed;
-     }
-     return this.location;
+   int getLocation(){
+       return this.location;
    }
-   public void display(){
-     String temp = "";
-     if (this.movingForward = true) {
-       temp = "forward"; 
-     }else if (this.movingForward = false) {
-       temp = "backwards";
-     }
+   //method sets the current speeed of car to be the object maxSpeed
+   void go(){
+     this.currentSpeed = this.maxSpeed;  
+   }
+   //method sets the current speed of the car to 0
+   void stop(){
+       this.currentSpeed = 0;
+   }
+   //method changes the boolean movingForward to be the opposit
+   void turnAround(){
+       this.movingForward = !this.movingForward; 
+       this.changedDirection = true;
+   }
+   //adds or subtracts the currentSpeed from the location
+   void continu(){
+        if(movingForward){
+            this.location += this.currentSpeed;
+        }
+        if(movingForward){
+            this.location -= this.currentSpeed;
+        }
+    }
+   //prints the name of the car, current speed, direction and location
+   void display(){
+       String temp = "";
+       if (movingForward) {
+           temp = "forward";
+       } else if (!movingForward){
+           temp = "backwards";
+       } 
        System.out.println(this.model + ": Located at " + this.location + 
-               ", fasing " + temp + " and moving at " + this.currentSpeed + " speed.");
+               ", fasing " + temp + " and mooving at " + this.currentSpeed + " speed.");
    }
-  
 }
